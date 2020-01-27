de4e86a2b899f1b6af1de106c15cbc60950d9e6b
Nate White <nate@streamlit.io>
2020-01-27 14:41:41 -0800

    Add shocks

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 import HARK as hark
 
-from HARK.ConsumptionSaving.ConsIndShockModel import PerfForesightConsumerType
+from HARK.ConsumptionSaving import ConsIndShockModel
 from HARK.utilities import plotFuncs
 
 
@@ -40,30 +40,61 @@ baseParams = {
     "AgentCount": 10000,
 }
 
-paramsList = []
+examples = []
 for i in range(parameterSets):
     st.markdown(f"#### Parameter Set {i + 1}:")
     params = copy.deepcopy(baseParams)
     params["Rfree"] = st.slider(
-        "Interest factor", 1.0, 1.5, 1.03 + i * .03, key=f"rfree{i}")
+        "Interest factor", 1.0, 1.5, 1.03 + i * 0.03, key=f"rfree{i}"
+    )
     params["PermGroFac"] = [
-        st.slider("Growth factor", 1.0, 1.2, 1.01 + i * .03, key=f"growth{i}")
+        st.slider("Growth factor", 1.0, 1.2, 1.01 + i * 0.03, key=f"growth{i}")
     ]
-    paramsList.append(params)
+    shocks = st.checkbox("Apply income shocks?", key=f"shocks{i}")
+    if shocks:
+        params["PermShkStd"] = [
+            st.slider(
+                "Permanent Income Shock", 0.0, 0.5, 0.1, key=f"PermShkStd{i}"
+            )
+        ]
+        params["PermShkCount"] = 7
+        params["TranShkStd"] = [
+            st.slider(
+                "Transitory Income Shock", 0.0, 0.5, 0.1, key=f"TranShkStd{i}"
+            )
+        ]
+        params["TranShkCount"] = 7
+        params["T_retire"] = 0
+        params["UnempPrb"] = 0.05
+        params["IncUnemp"] = 0.3
+        params["UnempPrbRet"] = 0.0
+        params["IncUnempRet"] = 0.0
 
-examples = []
-for i, params in enumerate(paramsList):
-    example = PerfForesightConsumerType(**params)
+        params.update(
+            {
+                "BoroCnstArt": 0.0,
+                "vFuncBool": False,
+                "CubicBool": False,
+                "aXtraMin": 0.001,
+                "aXtraMax": 50.0,
+                "aXtraNestFac": 3,
+                "aXtraCount": 48,
+                "aXtraExtra": [None],
+            }
+        )
+    if shocks:
+        example = ConsIndShockModel.IndShockConsumerType(**params)
+    else:
+        example = ConsIndShockModel.PerfForesightConsumerType(**params)
     example.solve()
     examples.append(example)
-    st.markdown(f"#### Solution {i + 1}:")
     st.write(
-        f"This agent's human wealth is {example.solution[0].hNrm:.02f} times "
-        f"his current income level."
+        f"**This agent's human wealth is {example.solution[0].hNrm:.02f} times "
+        f"his current income level.**"
     )
     st.write(
-        f"This agent's consumption function is defined (consumption is "
-        f"positive) down to m_t = {example.solution[0].mNrmMin:.02f}"
+        f"**This agent's consumption function is defined (consumption is "
+        f"positive) down to m_t = {example.solution[0].mNrmMin:.02f}**"
     )
 
 st.markdown("### Plot")

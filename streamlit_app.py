9b406b94ceec44c6a650f9e8c68b8d3b1203cfd9
Nate White <nate@streamlit.io>
2020-01-27 13:28:37 -0800

    Add inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,14 @@ from HARK.ConsumptionSaving.ConsIndShockModel import PerfForesightConsumerType
 st.header("Hello Hark!")
 
 PF_dictionary = {
-    'CRRA' : 2.5,
-    'DiscFac' : 0.96,
-    'Rfree' : 1.03,
-    'LivPrb' : [0.98],
-    'PermGroFac' : [1.01],
-    'T_cycle' : 1,
-    'cycles' : 0,
-    'AgentCount' : 10000
+    "CRRA": st.slider("Risk aversion (CRRA)", 0.0, 10.0, 2.5),
+    "DiscFac": st.slider("Discount factor", 0.0, 1.0, 0.96),
+    "Rfree": 1.03,
+    "LivPrb": [st.slider("Survival probability", 0.0, 1.0, 0.98)],
+    "PermGroFac": [1 + st.slider("Growth factor", 0.0, 0.2, 0.01)],
+    "T_cycle": 1,
+    "cycles": 0,
+    "AgentCount": 10000,
 }
 
 PFexample = PerfForesightConsumerType(**PF_dictionary)
@@ -22,9 +22,11 @@ PFexample.solve()
 
 # PFexample.solution[0].cFunc
 
-humanWealth = PFexample.solution[0].hNrm
-mMinimum = PFexample.solution[0].mNrmMin
-st.write(f"This agent's human wealth is {PFexample.solution[0].hNrm:.02f} "
-         f"times his current income level.")
-st.write(f"This agent's consumption function is defined (consumption is "
-         f"positive) down to m_t = {PFexample.solution[0].mNrmMin:.02f}")
+st.write(
+    f"This agent's human wealth is {PFexample.solution[0].hNrm:.02f} times "
+    f"his current income level."
+)
+st.write(
+    f"This agent's consumption function is defined (consumption is positive) "
+    f"down to m_t = {PFexample.solution[0].mNrmMin:.02f}"
+)
